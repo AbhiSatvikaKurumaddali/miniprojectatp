d2d9a05836dc9901d8390722eedad9c4337e7300
AbhiSatvikaKurumaddali <kurumaddali1201@gmail.com>
2026-05-09 16:44:13 +0530

    Update authRoutes.js

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -3,7 +3,6 @@ import { registerUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
-// ✅ Define the register endpoint
 router.post("/register", registerUser);
 
 export default router;
