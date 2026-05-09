ab6cc6d504f3fc546f76fbe6a5dff3d4c012623a
AbhiSatvikaKurumaddali <kurumaddali1201@gmail.com>
2026-05-09 18:25:11 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -9,9 +9,8 @@ import employeeRoutes from "./routes/employeeRoutes.js";
 dotenv.config();
 const app = express();
 
-
 app.use(cors({
-  origin: "https://miniprojectatp-2.onrender.com", 
+  origin: "https://miniprojectatp-2.onrender.com", // your frontend link
   credentials: true
 }));
 
@@ -19,7 +18,7 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
+  .catch(err => console.error("MongoDB connection error:", err));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/employees", employeeRoutes);
