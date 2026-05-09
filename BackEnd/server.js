739dcfdddf9c19eb1eeadad1eb4951685f3df0dd
AbhiSatvikaKurumaddali <kurumaddali1201@gmail.com>
2026-05-09 11:59:31 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,42 +1,38 @@
 import exp from "express";
+import {connect} from "mongoose";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-import { config } from "dotenv";
-import { employee } from "./API/employee.js";
+import {config} from "dotenv";
+import {employee} from "./API/employee.js";
 import cors from "cors";
-
-config();
-
-const app = exp();
-
+const app=exp()
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
-  })
+    origin: ["http://localhost:5173",
+            "https://atp-2opl.vercel.app"],
+  }),
 );
+app.use(exp.json())
 
-app.use(exp.json());
-app.use(cookieParser());
-
-let port = process.env.PORT || 3000;
-
+app.use(cookieParser())
+let port=3000
 app.use("/employee-api", employee);
-
-
-async function connectDB() {
-  try {
-    await mongoose.connect(process.env.DB_URL); 
-    console.log("MongoDB connected");
-    app.listen(port, () => console.log(`Server running on port ${port}`));
-  } catch (err) {
-    console.log("DB connection error:", err.message);
-  }
-}
-connectDB();
-
-
+async function connectDB(){
+    try{
+        await mongoose.connect(process.env.DB_url);
+        console.log("mongodb connected")
+        app.listen(port,()=>console.log(`server running on ${port}`))
+        
+    }
+    catch(err){
+        console.log(err)
+    }
+};
+connectDB()
+//error handling middleware
 app.use((err, req, res, next) => {
-  console.log("Error in middleware:", err.message);
+  console.log("err in middleware:", err.message);
+
   res.status(err.status || 500).json({
     message: "error",
     reason: err.message,
