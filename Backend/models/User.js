ef8b6fc238d83bd8133fb1a8420dae800dd27346
AbhiSatvikaKurumaddali <kurumaddali1201@gmail.com>
2026-05-09 18:24:22 +0530

    Update User.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   phone: String,
   password: { type: String, required: true },
