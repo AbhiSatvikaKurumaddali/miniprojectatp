669d9b21ab217beef18d2b6ec33d6fb52d378669
AbhiSatvikaKurumaddali <kurumaddali1201@gmail.com>
2026-05-09 16:32:25 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -12,15 +14,13 @@ app.use(cors({
   origin: "https://miniprojectatp-2.onrender.com"
 }));
 
-// Example route
-app.post("/api/auth/register", (req, res) => {
-  const { name, email, phone, password, role } = req.body;
-  if (!name || !email || !password) {
-    return res.status(400).json({ message: "Missing required fields" });
-  }
-  // TODO: Save user to DB (MongoDB)
-  res.status(201).json({ message: "User registered successfully" });
-});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB error:", err));
+
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
