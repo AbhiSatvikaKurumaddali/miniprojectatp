41c4a524fd458c72d1782985ef5999269ece8c80
AbhiSatvikaKurumaddali <kurumaddali1201@gmail.com>
2026-05-09 16:07:40 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,21 @@
 import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
 import cors from "cors";
-
-import authRoutes from "./routes/authRoutes.js";
-import employeeRoutes from "./routes/employeeRoutes.js";
-
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
 
-app.use("/api/auth", authRoutes);
-app.use("/api/employees", employeeRoutes);
+app.use(cors({
+  origin: "https://miniprojectatp-2.onrender.com"
+}));
+
+
+app.get("/api/employees", (req, res) => {
+  res.json({ message: "Employees list working!" });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
