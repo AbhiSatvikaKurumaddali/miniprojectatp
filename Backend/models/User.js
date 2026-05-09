fe6a2bd07e3e7aa9ddeda296c93963a38c4b5f34
AbhiSatvikaKurumaddali <kurumaddali1201@gmail.com>
2026-05-09 17:57:30 +0530

    Update User.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true },
-  phone: { type: String, required: true },
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: String,
   password: { type: String, required: true },
   role: { type: String, enum: ["employee", "admin"], default: "employee" }
 });
