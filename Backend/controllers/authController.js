149d79923695d344355725464d4d43c25b7644ec
AbhiSatvikaKurumaddali <kurumaddali1201@gmail.com>
2026-05-18 14:05:18 +0530

    Reformat authController.js for consistency

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -31,14 +31,14 @@ export const loginUser = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
 
-    // ✅ Generate JWT
+    
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
 
-    res.json({ token });   // <-- This is what you need
+    res.json({ token });   
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
